4e6b08b77ff96ee1d8c2956441891626af424d3e
OlgaLiV <9755609@gmail.com>
2020-08-07 04:50:34 +0300

    Checkbox and Radio buttons

---
+++ com/syntax/class04/LoginAndPassword.java
@@ -0,0 +1,29 @@
+package com.syntax.class04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginAndPassword {
+	public static String url = "http://166.62.36.207/humanresources/symfony/web/index.php/auth/login";
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id = 'txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@id = 'txtPassword']")).sendKeys("Hum@nhrm123");
+		driver.findElement(By.cssSelector("input#btnLogin")).click();
+		WebElement logo = driver.findElement(By.xpath("//img[@alt = 'OrangeHRM']"));
+		if(logo.isDisplayed()) {
+			System.out.println("Logo is displayed");
+		}else {
+			System.out.println("Logo is NOT displayed");
+		}
+		
+
+	}
+}
