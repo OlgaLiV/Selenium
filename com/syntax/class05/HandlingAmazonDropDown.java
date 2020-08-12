5f7495d526eed0f1bd70da50d80974c5cf6c57e1
OlgaLiV <9755609@gmail.com>
2020-08-12 03:10:18 +0300

    Handling dropdowns

---
+++ com/syntax/class05/HandlingAmazonDropDown.java
@@ -0,0 +1,34 @@
+package com.syntax.class05;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class HandlingAmazonDropDown {
+
+	public static String url = "https://www.amazon.com/";
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+
+		WebElement categoriesDD = driver.findElement(By.id("searchDropdownBox"));
+		Select select = new Select(categoriesDD);
+
+		List<WebElement> options = select.getOptions();
+		int s = options.size();
+		for (WebElement option : options) {
+			String text = option.getText();
+			System.out.println(text);
+		}
+		
+		select.selectByValue("search-alias=fashion-mens-intl-ship");
+
+	}
+
+}
